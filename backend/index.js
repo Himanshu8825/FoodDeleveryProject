bc179302d4167812055580a79ff6f9bc0bb87793
Suraj Verma <121680980+Himanshu8825@users.noreply.github.com>
2024-05-13 11:39:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(
     origin: [
       "http://localhost:3001",
       "http://localhost:5173",
-      "https://fooddelevery-z5mv.onrender.com/",
+      "https://fooddelevery-z5mv.onrender.com",
     ],
   })
 );
